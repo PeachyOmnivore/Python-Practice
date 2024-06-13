875e6c057ff783693cfd912c42bf9601d2d593de
Lukas <lad_777@hotmail.com>
2024-06-13 12:23:12 +0100

    build: Experimenting with Python and interpreters

---
+++ index.py
@@ -0,0 +1,11 @@
+print("Hello World")
+
+#A small program to add two numbers
+num1 = 10
+num2 = 30
+
+#Adds the two numbers together
+sum = num1 + num2
+
+#Displays the sum on screen
+print("The total of your two numbers is:", sum)
