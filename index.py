53e16e8892b67a811e26ea040e7a0bfc14fb735a
Lukas <lad_777@hotmail.com>
2024-06-19 14:48:57 +0100

    Added some more functions to playa round with.

--- index.py
+++ index.py
@@ -1,3 +1,6 @@
+from math import floor
+
+
 print("Hello World")
 
 #A small program to add two numbers
@@ -32,3 +35,42 @@ def hint_username(username):
 
 hint_username("12")
 hint_username("123")
+
+def numcheck(number):
+    if number > 11: 
+        print(0)
+    elif number != 10:
+        print(1)
+    elif number >= 20 or number < 12:
+         print(2)
+    else:
+        print(3)
+
+numcheck(10)
+
+
+# Playing with modulos and interesting formats.
+def calculate_storage(filesize):
+    block_size = 4096
+    # Use floor division to calculate how many blocks are fully occupied
+    full_blocks = filesize / block_size
+    # Use the modulo operator to check whether there's any remainder
+    partial_block_remainder = full_blocks % 1
+    # Depending on whether there's a remainder or not, return
+    # the total number of bytes required to allocate enough blocks
+    # to store your data.
+    if partial_block_remainder > 0:
+        return block_size * floor(full_blocks) + block_size
+    return block_size * floor(full_blocks)
+
+print(calculate_storage(1))    # Should be 4096
+print(calculate_storage(4096)) # Should be 4096
+print(calculate_storage(4097)) # Should be 8192
+print(calculate_storage(6000)) # Should be 8192
+
+
+# Double functions
+def sum(x, y):
+        return(x+y)
+
+print(sum(sum(1,2), sum(3,4)))
