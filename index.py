2a83bdbfe31ec3727419aa625ef64fd7a991f1d2
Lukas <lad_777@hotmail.com>
2024-08-01 14:14:20 +0100

    feat: create staircase code.

--- index.py
+++ index.py
@@ -218,3 +218,18 @@
 # replace_domain('lad_777@mail.com', 'mail.com', 'hotmail.com')
 
 #--------------------------------------------------------------------------------------------------------------------------------
+
+def create_staircase(nums):
+  while len(nums) != 0:
+    step = 1
+    subsets = []
+    if len(nums) >= step:
+      subsets.append(nums[0:step])
+      nums = nums[step:]
+      step += 1
+    else:
+      return False
+
+  return subsets
+
+print(create_staircase([1, 2, 3, 4, 5, 6, 7]))
